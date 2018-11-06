ce2359f98da03cd08a37c6cad1069979966317da
Abhilash Chaturvedi <abhilash.chaturvedi@xooa.com>
2018-11-06 12:03:04 +0530

    made changes for dropbox

--- vtestnode/index.js
+++ vtestnode/index.js
@@ -9,8 +9,6 @@ const shim = require("fabric-shim");
 
 const ClientIdentity = require("fabric-shim").ClientIdentity;
 
-const Stub = require("fabric-shim").Stub;
-
 async function allowAppAccess(stub) {
   let cid = new ClientIdentity(stub); // "stub" is the ChaincodeStub object passed to Init() and Invoke() methods
   let [AppId, Version] = process.env.CORE_CHAINCODE_ID_NAME.split(":");
@@ -34,70 +32,41 @@ let Chaincode = class {
     console.info("=========== Instantiated fabcar chaincode ===========");
     return shim.success();
   }
-
   // The Invoke method is called as a result of an application request to run the Smart Contract
   // 'fabcar'. The calling application program has also specified the particular smart contract
   // function to be called, with arguments
   async Invoke(stub) {
     let ret = stub.getFunctionAndParameters();
     console.info(ret);
+    var counter = 0;
     let method = this[ret.fcn];
-    try {
-      if (!method) {
-        console.error("no function of name:" + ret.fcn + " found");
-        throw new Error("Received unknown function " + ret.fcn + " invocation");
-      }
-    } catch (err) {
-      console.log(err);
-      let shimError = shim.error(err);
-      shimError.status = 404;
-      shimError.message = err.message;
-      console.log(shimError);
-      return shimError;
-    }
 
     try {
       //check account level access
       await allowAccountAccess(stub);
       //check  app level access
       await allowAppAccess(stub);
-
+      if (!method) {
+        console.error("no function of name:" + ret.fcn + " found");
+        throw new Error("Received unknown function " + ret.fcn + " invocation");
+      }
       let payload = await method(stub, ret.params);
       return shim.success(payload);
     } catch (err) {
       console.log(err);
-      let shimError = shim.error(err);
-      shimError.status = Stub.RESPONSE_CODE.ERRORTHRESHOLD;
-
-      shimError.message = err.message;
-      return shimError;
+      return shim.error(err);
     }
   }
 
-  async get(stub, args) {
-    if (args.length != 1) {
+  async saveContentHash(stub, args) {
+    if (args.length < 1) {
       throw new Error(
-        "Incorrect number of arguments. Expecting CarNumber ex: CAR01"
+        "Incorrect argument."
       );
     }
-    let carNumber = args[0];
-
-    let carAsBytes = await stub.getState(carNumber); //get the car from chaincode state
-    if (!carAsBytes || carAsBytes.toString().length <= 0) {
-      throw new Error(carNumber + " does not exist: ");
-    }
-    console.log(carAsBytes.toString());
-    return carAsBytes;
-  }
-
-  async set(stub, args) {
-    console.info("============= START : Create Car ===========");
-    if (args.length != 2) {
-      throw new Error("Incorrect number of arguments. Expecting 5");
-    }
-    stub.setEvent("putstate", Buffer.from(args[1]));
+   
     await stub.putState(args[0], Buffer.from(args[1]));
-    console.info("============= END : Create Car ===========");
+    return args[0];
   }
 };
 
